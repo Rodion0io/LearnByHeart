cca6c446ca4f6c2d82280f761a60f0eb98d5a19a
Rodion <79993057+Rodion0io@users.noreply.github.com>
2022-10-22 14:01:53 +0700

    replicas.py added, bot.py modified

--- bot.py
+++ bot.py
@@ -2,13 +2,18 @@ from aiogram import types,Bot
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
 import config
+from random import choice
+import replicas
+import keyboards as kb
 
 bot = Bot(token=config.token)
 dp = Dispatcher(bot)
 
-@dp.message_handler(commands='start')
-async def start_bot(message: types.Message):
-    await bot.send_message(message.from_user.id, f'Привет {message.from_user}')
+@dp.message_handler(commands=['start'])
+async def start_bot(message:types.Message):
+    await message.reply(choice(replicas.messages['greeting']),reply_markup=kb.main_keyboard)
+
+
 
 
 if __name__ == '__main__':
---
+++ config.py
@@ -0,0 +1,1 @@
+token = '5564342843:AAFnq_-F3vii5OC6dToQtwJUqrn54J0xZHU'
